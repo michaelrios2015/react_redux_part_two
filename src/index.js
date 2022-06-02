61bd90ec8b759b8a8f4a27fe2f5eeba4a0b8a14f
Michael G Rios <michael.rios093@gmail.com>
2022-06-02 17:17:16 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { render } from 'react-dom';
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            users: [],
+            loading: true
+        };
+    }
+
+    async componentDidMount(){
+        this.setState({ 
+            users: (await axios.get('/api/users')).data,
+            loading: false
+         });
+    }
+    
+    render(){
+        const { users, loading } = this.state
+        if (loading){
+            return '.....loading';
+        }
+        return (
+                <ul>
+                    {
+                        users.map( user => {
+                            return (
+                                <li key = { user.id }>
+                                    { user.name }
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+        );
+    }
+}
+
+
+import { createRoot } from 'react-dom/client';
+const container = document.querySelector('#root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+
+// not the way they do it anymore 
+// render(<App />, document.querySelector('#root'));
